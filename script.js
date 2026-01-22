48c85f1aebdc73417fa0d9e1a48782fd5f82d640
RinorSelmanii <rinorselmani99@gmail.com>
2026-01-23 00:10:42 +0100

    if statement for name

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 document.getElementById("event").addEventListener("submit",function(e){
     e.preventDefault();
 
-    
+    let name = document.getElementById("name").trim.value();
+    let email = document.getElementById("email").trim.value();
+    let fb = document.getElementById("feedback").trim.value();
+
+    let ok = true;
+
+    if(!/^[a-zA-Z\s]+$/ .test(name) || name === "" ){
+        document.getElementById("errE").innerText = "Invalid Name";
+        ok = false;
+    }
 });
