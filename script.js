b30cf0f3fff7663a331662ce65ce23fcf2e35239
RinorSelmanii <rinorselmani99@gmail.com>
2026-01-23 00:18:06 +0100

    if's for email,feedback

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 document.getElementById("event").addEventListener("submit",function(e){
     e.preventDefault();
 
-    let name = document.getElementById("name").trim.value();
-    let email = document.getElementById("email").trim.value();
-    let fb = document.getElementById("feedback").trim.value();
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let fb = document.getElementById("feedback").value.trim();
 
     let ok = true;
 
     if(!/^[a-zA-Z\s]+$/ .test(name) || name === "" ){
-        document.getElementById("errE").innerText = "Invalid Name";
+        document.getElementById("errN").innerText = "Invalid Name";
         ok = false;
+    }
+    if(email === ""){
+        document.getElementById("errE").innerText = "Invalid E-mail";
+        ok = false;
+    }
+    if(fb === "" ){
+        document.getElementById("errF").innerText = "Invalid feedback";
+        ok = false;
+    }
+    if(ok){
+        document.getElementById("suces").innerText = "Submited!";
     }
 });
